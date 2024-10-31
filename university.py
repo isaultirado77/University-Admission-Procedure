c6254313f71126a12eb7c488a410b74ff1a1e9c4
Isaul <tiradolisaul@gmail.com>
2024-10-30 20:35:31 -0700

    init stage 3

--- university.py
+++ university.py
@@ -1,30 +1,5 @@
-def read_grades() -> list:
-    while True:
-        try:
-            grades = [float(input()) for _ in range(3)]
-            return grades
-        except ValueError:
-            print("Error: Enter a number. ")
-
-
-def calculate_mean_grade(grades: list = None) -> float:
-    average = sum(grades) / len(grades)
-    return average
-
-
-def determine_status_admission(mean: float = 50.) -> str:
-    if mean >= 60:
-        return 'Congratulations, you are accepted!'
-    else:
-        return 'We regret to inform you that we will not be able to offer you admission.'
-
-
 def main():
-    grades = read_grades()
-    mean = calculate_mean_grade(grades)
-    print(mean)
-    status_admission = determine_status_admission(mean)
-    print(status_admission)
+    pass
 
 
 if __name__ == '__main__':
