d165c678021b48462d25d0baeb8f1c4d06e4daca
Isaul <tiradolisaul@gmail.com>
2024-10-30 18:43:32 -0700

    read_grades & calculate_mean functions: implemented

--- university.py
+++ university.py
@@ -1,5 +1,23 @@
+def read_grades() -> list:
+    while True:
+        try:
+            grades = [float(input()) for _ in range(3)]
+            return grades
+        except ValueError:
+            print("Error: Enter a number. ")
+
+
+def calculate_mean_grade(grades: list = None) -> float:
+    average = sum(grades) / len(grades)
+    return average
+
+
 def main():
-    pass
+    grades = read_grades()
+    mean = calculate_mean_grade(grades)
+    print(mean)
+    print('Congratulations, you are accepted!')
+
 
 
 if __name__ == '__main__':
