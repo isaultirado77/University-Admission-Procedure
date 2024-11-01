f0beeb6227148ed35728095537086ecf0d88f03b
Isaul <tiradolisaul@gmail.com>
2024-10-31 20:02:50 -0700

    selection mechanism implemented: working

--- university.py
+++ university.py
@@ -14,15 +14,14 @@ def read_applicants_data(applicants_number: int = 0) -> dict:
     for _ in range(applicants_number):
         split_line = input().split()
         full_name = f'{split_line[0]} {split_line[1]}'
-        gpa = round(float(split_line[2]))
+        gpa = float(split_line[2])
         data[full_name] = gpa
 
     return data
 
 
 def get_successful_applicants(data: dict, total_applicants: int):
-    sorted_applicants = dict(sorted(data.items(), key=lambda item: (item[1], item[0])),
-                             reverse=True)  # Sort by gpa(item[1]) then by name(item[0])
+    sorted_applicants = dict(sorted(data.items(), key=lambda item: (-item[1], item[0])))  # Sort by gpa(item[1]) then by name(item[0])
     successful_applicants = [item[0] for item in sorted_applicants.items()]
     return successful_applicants[:total_applicants]
 
@@ -32,6 +31,7 @@ def main():
     total_accepted_applicants = read_positive_integer()
     applicants_data = read_applicants_data(total_applicants)
     successful_applicants = get_successful_applicants(applicants_data, total_accepted_applicants)
+    print('Successful applicants:')
     for applicant in successful_applicants:
         print(applicant)
 
