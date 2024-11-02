1a8bd118125957c7f9a7c4e4e6b5a06604d2b2e6
Isaul <tiradolisaul@gmail.com>
2024-11-01 17:00:14 -0700

    ready to develop stage 4

--- university.py
+++ university.py
@@ -9,31 +9,23 @@ def read_positive_integer() -> int:
             print("\nError: Your input must be an positive integer.\n")
 
 
-def read_applicants_data(applicants_number: int = 0) -> dict:
-    data = {}
-    for _ in range(applicants_number):
-        split_line = input().split()
-        full_name = f'{split_line[0]} {split_line[1]}'
-        gpa = float(split_line[2])
-        data[full_name] = gpa
-
-    return data
+def read_applicants_data(applicants_number: int = 0):
+    pass
 
 
-def get_successful_applicants(data: dict, total_applicants: int):
-    sorted_applicants = dict(sorted(data.items(), key=lambda item: (-item[1], item[0])))  # Sort by gpa(item[1]) then by name(item[0])
+def get_successful_applicants(data: dict, accepted_applicants: int):
+    sorted_applicants = dict(
+        sorted(data.items(), key=lambda item: (-item[1], item[0])))  # Sort by gpa(item[1]) then by name(item[0])
     successful_applicants = [item[0] for item in sorted_applicants.items()]
-    return successful_applicants[:total_applicants]
+    return successful_applicants[:accepted_applicants]
 
 
-def main():
-    total_applicants = read_positive_integer()
-    total_accepted_applicants = read_positive_integer()
-    applicants_data = read_applicants_data(total_applicants)
-    successful_applicants = get_successful_applicants(applicants_data, total_accepted_applicants)
-    print('Successful applicants:')
-    for applicant in successful_applicants:
-        print(applicant)
+def run() -> None:
+    pass
+
+
+def main() -> None:
+    pass
 
 
 if __name__ == '__main__':
