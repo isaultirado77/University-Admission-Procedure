dbf6b263063edb1c1384ad5fb5b97b256702b986
Isaul <tiradolisaul@gmail.com>
2024-11-03 18:01:23 -0700

    Applicant class implemented

--- university.py
+++ university.py
@@ -1,3 +1,17 @@
+class Applicant:
+    def __init__(self, name: str, gpa: float, priorities: tuple):
+        self.name = name
+        self.gpa = gpa
+        self.priorities = priorities
+
+    def __str__(self):
+        return f'{self.name} {self.gpa} {self.priorities}'
+
+
+class Department:
+    pass
+
+
 def main() -> None:
     pass
 
